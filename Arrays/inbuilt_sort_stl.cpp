ccab7007b6a2bea8b7ac1132717603cff9b39c1c
asksaurabh <asksaurabh101@gmail.com>
2020-07-16 11:18:41 +0530

    Inbuilt sort in C++

---
+++ Arrays/inbuilt_sort_stl.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+bool compare(int a, int b){     //This function decides how to compare two integers.	
+
+	cout<<"Comparing "<<a<<" and "<<b<<endl;
+	return a > b;
+}
+
+int main(){
+
+	int a[100], n;
+
+	cout<<"Enter the size of the array:";
+	cin>>n;
+
+	cout<<"Enter the array:";
+	for(int i=0; i<n; i++)
+		cin>>a[i];
+
+	//Inbuilt sort function..
+
+	sort(a,a+n,compare);              //Alter the compare function to see ascending and descending order
+
+	cout<<"Array after inbuilt sort:";
+	for(int i=0; i<n; i++)
+		cout<<a[i]<<" ";
+
+	return 0;
+}
