8a91e72f2c61e50787d6e98bca6ea6c8568672bd
asksaurabh <asksaurabh101@gmail.com>
2020-07-17 20:11:37 +0530

    Kadane's Algorithm

---
+++ Arrays/kadane_algo.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	int a[100],n,left=0,right=0,final_left=0;
+
+	cout<<"Enter the size of the array:";
+	cin>>n;
+
+	cout<<"Enter the array:";
+	for(int i=0; i<n; i++)
+		cin>>a[i];
+
+	int cs = 0;
+	int ms = 0;
+
+	for(int i=0; i<n; i++){
+
+		cs = cs + a[i];
+
+		if(cs < 0){
+
+			cs = 0;
+			left = i+1;
+		}
+
+		if(cs > ms){
+			ms = cs;
+
+			final_left = left;
+			right = i;
+		}
+	}
+
+	cout<<"Maximum subarray sum is:"<<ms<<endl;
+	cout<<"Maximum subarray is:";
+	for(int i = final_left ; i<= right; i++)
+		cout<<a[i]<<" ";
+
+	return 0;
+}
