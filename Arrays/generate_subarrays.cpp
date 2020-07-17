64a42c53deddf6bc366c43c4b507f260464fe63d
asksaurabh <asksaurabh101@gmail.com>
2020-07-17 14:09:58 +0530

    Subarray generation

---
+++ Arrays/generate_subarrays.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int a[100], n;
+
+	cout<<"Enter the size of the array:";
+	cin>>n;
+
+	cout<<"Enter the array:";
+	for(int i=0; i<n; i++)
+		cin>>a[i];
+
+	cout<<"Subarrays are:"<<endl;
+
+	for(int i=0; i<n; i++){
+
+		for(int j=i; j<n; j++){
+
+			for(int k=i; k<=j; k++){
+
+				cout<<a[k]<<" ";
+			}
+			cout<<endl;
+		}
+		//cout<<endl;
+	}
+
+	return 0;
+}
