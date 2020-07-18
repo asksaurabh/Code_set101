4087a7772de7954c68bc14ff54191d78b0a961d7
asksaurabh <asksaurabh101@gmail.com>
2020-07-18 13:07:30 +0530

    Pair sum problem

---
+++ Arrays/two_pointer_approach.cpp
@@ -0,0 +1,40 @@
+//Given a sorted array, find pair of elements that sums to K(given)
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	int a[100], n,k;
+	cout<<"Enter the size of the array:";
+	cin>>n;
+
+	cout<<"Enter the array:";
+	for(int i=0; i<n; i++)
+		cin>>a[i];
+
+	cout<<"Enter the sum:";
+	cin>>k;
+
+	int i = 0;
+	int j = n-1;
+	
+	cout<<"All possible pairs are: ";
+	while(i<j){
+
+		if(a[i] + a[j] == k){
+			
+			cout<<"("<<a[i]<<","<<a[j]<<")"<<" ";
+			i++;
+			j--;
+		}
+
+		else if((a[i] + a[j]) < k)
+			i++;
+
+		else
+			j--;
+
+	}
+
+	return 0;
+}
