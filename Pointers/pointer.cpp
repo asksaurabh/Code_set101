b205d88f04fadfe4b1b68bb0174e045b0f58f403
asksaurabh <asksaurabh101@gmail.com>
2020-07-24 12:04:00 +0530

    Pointer introduction

---
+++ Pointers/pointer.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	int x = 10;
+
+	cout<< &x <<endl;
+
+	float y = 10.5;
+	cout<< &y <<endl;             //In hexadecimal format.
+
+	char ch = 'A';
+
+	cout<< &ch<<endl;   //Exception it doesn't work as expected.. due to operator overloading .
+
+	// to fix this typecast char * to void * (data type of &ch was char *)
+
+	cout<< (void *)&ch<<endl;
+	cout<<typeid(&ch).name()<<endl;         //Tells u the type of variable in c++
+
+	// Pointer
+	//Storing the address of variable
+	int * xptr = &x;
+
+	cout<<xptr<<endl;
+	cout<< &x <<endl;
+
+	//u can re-assign the variable as well.
+
+	int z = 20;
+
+	xptr = &z;
+
+	cout<<xptr<<endl;
+	cout<< &z <<endl;
+
+
+
+	return 0;
+}
