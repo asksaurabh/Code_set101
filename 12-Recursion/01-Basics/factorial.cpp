632a57b981f73752fa27e87ce29b76d42f91209c
asksaurabh <asksaurabh101@gmail.com>
2020-08-18 21:09:44 +0530

    01 - Factorial using recursion

---
+++ 12-Recursion/01-Basics/factorial.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+	//Base case
+	if(n == 0 || n == 1){
+		return 1;
+	}
+
+	//Recursive case.
+	int ans = n * factorial(n-1);
+	return ans;
+}
+int main(){
+
+	int n;
+
+	cout<<"Enter the number you want the factorial of:";
+	cin>>n;
+
+	cout<<"Factorial of given number is:"<<factorial(n);
+	return 0;
+}
